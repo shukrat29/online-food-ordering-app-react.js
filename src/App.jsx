28327915a9ca3812b4eab4ae43e52fa2756632ca
Shukra Tamang <shukratamang.npl@gmail.com>
2024-09-10 07:40:52 +1000

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="font-bold underline text-center">Welcome to Foodbazaar</h1>
+    </div>
+  );
+}
+
+export default App;
