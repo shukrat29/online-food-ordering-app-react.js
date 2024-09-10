ba338133b8863d5f813536c8a2ac1632a98fbe00
Shukra Tamang <shukratamang.npl@gmail.com>
2024-09-11 09:36:33 +1000

    Top rated restaurants rendered using useState() hook

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 
 import { resList } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // state variable
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
   return (
     <>
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            //  filter logic here
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
 
       <div className="res-container">
-        {resList.map((restaurant) => (
-          <RestaurantCard resData={restaurant} />
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,7 @@ const Header = () => {
           <li>Home</li>
           <li>About Us</li>
           <li>Contact Us</li>
+          <li>Cart</li>
         </ul>
       </div>
     </div>
