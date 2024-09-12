e82aacb583bef5721faac00ac6dc37d1c6449dda
Shukra Tamang <shukratamang.npl@gmail.com>
2024-09-12 11:18:21 +1000

    live data fetched using useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 
-import { resList } from "../utils/mockData";
-import { useState } from "react";
+// import { resList } from "../utils/mockData";
+import { useEffect, useState } from "react";
 
 const Body = () => {
   // state variable
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.960059122809971&lng=77.57337538383284&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    // console.log(json);
+    setListOfRestaurants(
+      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
   return (
     <>
       <div className="filter">
@@ -19,7 +35,9 @@ const Body = () => {
             setListOfRestaurants(filteredList);
           }}
         >
-          Top Rated Restaurants
+          <h3 className="text-lg font-semibold">
+            Top Rated Restaurants (&gt;4.5 stars)
+          </h3>
         </button>
       </div>
 
